0a8afa71ed34c5b1f0269e9140bb41999320b49c
hazelln <134735578+hazelln@users.noreply.github.com>
2023-08-03 14:37:54 +1200

    Initial commit

---
+++ Scripts/Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+	
+	 
+
+func _on_OptionsButton_pressed():
+	pass
+
+
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+#get play button to work
+
+
+func _on_PlayButton_button_down():
+	get_tree().change_scene("res://Scenes/Game.tscn")
---
+++ Scripts/Options.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_Volume_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_Back_pressed():
+	pass
+	
