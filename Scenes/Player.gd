043aa2e0dda583599a287ce90017a8b08367b022
hazelln <134735578+hazelln@users.noreply.github.com>
2023-08-31 15:01:13 +1200

    Game assets

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -3,7 +3,12 @@ extends KinematicBody2D
 const GRAVITY = 1000
 var velocity = Vector2.ZERO
 
+var playername = "Munhu"
+
+
 func _physics_process(delta):
+	print("Hello there players<3")
+	print(playername)
 	velocity.x =  Input.get_axis("left","right") * 200
 	velocity.y +=  GRAVITY * delta
 	move_and_slide(velocity,Vector2.UP)
