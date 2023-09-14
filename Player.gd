e26e70e3ff045af111dd6e7de5d8be43b6dd3968
hazelln <134735578+hazelln@users.noreply.github.com>
2023-09-14 15:06:13 +1200

    Tileset added but incomplete

--- Player.gd
+++ Player.gd
@@ -3,14 +3,15 @@ extends KinematicBody2D
 const GRAVITY = 1000
 var velocity = Vector2.ZERO
 
-var playername = "Munhu"
+
 
 
 func _physics_process(delta):
-	print("Hello there players<3")
-	print(playername)
+
 	velocity.x =  Input.get_axis("left","right") * 200
 	velocity.y +=  GRAVITY * delta
 	move_and_slide(velocity,Vector2.UP)
 	if Input.is_action_just_pressed("jump"):
 			velocity.y = -200
+	
+	
