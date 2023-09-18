0005972aaa84c91f8f913a423d6294a41872eae7
hazelln <134735578+hazelln@users.noreply.github.com>
2023-09-18 23:13:17 +1200

    button font change

---
+++ Scripts/Player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+const GRAVITY = 1000
+var velocity = Vector2.ZERO
+
+
+
+
+func _physics_process(delta):
+
+	velocity.x =  Input.get_axis("left","right") * 200
+	velocity.y +=  GRAVITY * delta
+	move_and_slide(velocity,Vector2.UP)
+	if Input.is_action_just_pressed("jump"):
+			velocity.y = -200
+
+	#halothere
+
