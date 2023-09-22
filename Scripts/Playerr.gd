367083c49eb8656f56562c38c4bd460c816184f8
hazelln <134735578+hazelln@users.noreply.github.com>
2023-09-22 15:00:30 +1200

    idk

--- Scripts/Playerr.gd
+++ Scripts/Playerr.gd
@@ -5,6 +5,17 @@ var velocity = Vector2.ZERO
 var speed = 0
 onready var animation = $AnimatedSprite
 onready var sprite = $Sprite 
+#var start_position = Vector2.ZERO
+
+#func _ready():
+	#start_position = position
+
+#func _process(delta):
+		 #if position.y > 480 :
+			 #position = start_position
+
+#func respwan():
+	#position = start_position
 
 func _physics_process(delta):
 	# Check if the character is grounded (on the floor)
@@ -20,6 +31,7 @@ func _physics_process(delta):
 
 	# Calculate the horizontal velocity based on input
 	velocity.x = Input.get_action_strength("right") * 200 - Input.get_action_strength("left") * 200
+	velocity.y += GRAVITY * delta
 	
 	if velocity.x > 0:
 		$AnimatedSprite.flip_h = false
